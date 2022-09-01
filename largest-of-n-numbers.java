a7184d0ee4ded2afac9f577ad4ad7baa4c7b1d27
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-09-01 18:36:40 +0700

    Create largest-of-n-numbers.java
    program to find the largest of a given n number of values (integers)

---
+++ largest-of-n-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int max=0,a[],num;
+
+        System.out.println("Enter the number of values: ");
+        Scanner sc = new Scanner(System.in);
+        
+        num = sc.nextInt();
+        
+        System.out.println("Enter the numbers: ");
+       a = new int[num + 1];
+        for(int i=0; i<num;i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0; i<num;i++){
+            if(max<a[i]){
+                max=a[i];
+            }
+        }
+        System.out.println("the largest number is: "+ max);
+    }
+}
