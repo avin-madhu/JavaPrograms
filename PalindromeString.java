742ca92bd2dc9d0be89f4f558356b62631995210
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-09-18 22:16:00 +0700

    .

---
+++ PalindromeString.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class PalindromeString {
+    public static void main(String args[])
+    {
+       char string[]=new char[100];
+       int len;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the string length: ");
+       len = sc.nextInt();
+
+        System.out.println("Enter the string: ");
+
+       for(int i=0;i<len;i++)
+       {
+        
+       }
+       
+
+
+      
+       
+
+       
+
+
+
+    }
+  
+}
