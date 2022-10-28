cf9e8b5cbc742c2ab370e1c147d27f602a354411
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-10-28 22:49:22 +0700

    set3 exp 1

--- SET 3/Student.java
+++ SET 3/Student.java
@@ -1,6 +1,41 @@
-/* Program to create a student class with name,roll number and marks of 5 subjects.
-   Write functions to read and display the details */ 
-
+import java.util.Scanner;
 public class Student {
-    
+    String name ;
+    int rollNum;
+    int marks[]= new int[5];
+    Scanner sc = new Scanner(System.in);
+    void Read()
+    {
+
+      System.out.print("Enter the Student name: ");
+      name = sc.nextLine();
+      System.out.print("Enter the Student Roll Number: ");
+      rollNum = sc.nextInt();
+      System.out.print("Enter the Student Marks: ");
+      for(int i=0;i<5;i++)
+      {
+         marks[i]=sc.nextInt();
+      }
+
+    }
+    void Display()
+    {
+      System.out.println("Student Name: "+name);
+      System.out.println("Student Roll num: "+rollNum);
+      System.out.println("Student marks: ");
+      for(int i=0;i<5;i++)
+      {
+         int si=i+1;
+         System.out.println(si+"."+" "+marks[i]);
+      }
+    }
+
+    public static void main(String args[])
+    {
+       Student obj = new Student();
+       obj.Read();
+       obj.Display();
+    }
 }
+
+
