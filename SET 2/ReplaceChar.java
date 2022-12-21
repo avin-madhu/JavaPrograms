49cb0a56bb117eb5f2c691babc0275019c4d57e2
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2022-12-21 23:02:48 +0700

    Update ReplaceChar.java

--- SET 2/ReplaceChar.java
+++ SET 2/ReplaceChar.java
@@ -1,32 +1,33 @@
-// error code!!!
-
-
-import java.util.Scanner;
+import java.util.*;
 public class ReplaceChar {
-
-       public static void  main(String args[])
-       {
-        char ch1,ch2;
-        int flag=0,pos=0;
+    public static void main(String args[])
+    {
+        String strNew = new String();
         String str = new String();
-        System.out.print("Enter the string: ");
+        char ch,chNew;
+           
         Scanner sc = new Scanner(System.in);
+           
+        System.out.print("Enter the string: ");
         str = sc.nextLine();
-        System.out.print("Enter the character to be replaced");
-        ch1 = sc.next().charAt(0);
-        System.out.print("Enter the character to replace with");
-        ch2 = sc.next().charAt(0);
-       
+           
+        System.out.print("Enter the element to replace: ");
+        ch = sc.next().charAt(0);
+           
+        System.out.print("Enter the charcter to be replaced: ");
+        chNew = sc.next().charAt(0);
+           
         for(int i=0;i<str.length();i++)
         {
-            if(str.charAt(i)==ch1)
+            if(ch == str.charAt(i))
             {
-              str.replace(ch1 , ch2);
-              break;
+                 strNew = str.replace(str.charAt(i), chNew);
             }
         }
-       
-            System.out.println("The new string: "+str);
-       }
-        
+           
+        System.out.print("The new string is: "+strNew);
+
+    }
 }
+
+
